3c43ad54624b546a1c716a4dc3e512337a682113
Gerard Falco <gerardfp@users.noreply.github.com>
2021-03-26 17:34:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,82 @@
 const express = require('express');
-var uuid = require("uuid");
+const { v4: uuidv4,} = require('uuid');
 
 const PORT = process.env.PORT || 5000
 
  express()
-   .get('/:size?', (req, res) => {
+   .get('/robots/:size?', (req, res) => {
      if(!req.params.size) req.params.size = 25;
 
      let response = { response: []};
      for(let i=0; i<req.params.size; i++){
-       let id = uuid();
+       let id = uuidv4();
         response.response.push({
-          name: id,
-          src: 'https://robohash.org/{$id}'
+          id: id,
+          name: id.replace(/-/g, ' '),
+          shortname: id.replace(/-.*/, ''),
+          src: `https://robohash.org/${id}`,
+          likes: 0
         });
      }
     
     res.send(response);
    })
+   .get('/monsters/:size?', (req, res) => {
+    if(!req.params.size) req.params.size = 25;
+
+    let response = { response: []};
+    for(let i=0; i<req.params.size; i++){
+      let id = uuidv4();
+       response.response.push({
+         id: id,
+         name: id.replace(/-/g, ' '),
+         shortname: id.replace(/-.*/, ''),
+         src: `https://robohash.org/${id}?set=set2`,
+         likes: 0
+       });
+    }
+   
+   res.send(response);
+  })
+  .get('/kittens/:size?', (req, res) => {
+    if(!req.params.size) req.params.size = 25;
+
+    let response = { response: []};
+    for(let i=0; i<req.params.size; i++){
+      let id = uuidv4();
+       response.response.push({
+         id: id,
+         name: id.replace(/-/g, ' '),
+         shortname: id.replace(/-.*/, ''),
+         src: `https://robohash.org/${id}?set=set4`,
+         likes: 0
+       });
+    }
+   
+   res.send(response);
+  })
+  .get('/humans/:size?', (req, res) => {
+    if(!req.params.size) req.params.size = 25;
+
+    let response = { response: []};
+    for(let i=0; i<req.params.size; i++){
+      let id = uuidv4();
+       response.response.push({
+         id: id,
+         name: id.replace(/-/g, ' '),
+         shortname: id.replace(/-.*/, ''),
+         src: `https://robohash.org/${id}?set=set5`,
+         likes: 0
+       });
+    }
+   
+   res.send(response);
+  })
+
+  .get('/likes/:id?', (req, res) => {
+   res.send({
+     likes: Math.floor(Math.random()*2000 + 1)
+   });
+  })
+
    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
