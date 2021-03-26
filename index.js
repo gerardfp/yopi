4a8da95456b0d78495f96d35059939b41a9cad2a
gerardfp <gerardfp@gmail.com>
2021-03-26 16:22:17 +0100

    .

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+var uuid = require("uuid");
 
 const PORT = process.env.PORT || 5000
 
  express()
-   .get('/', (req, res) => res.send('hola'))
+   .get('/:size?', (req, res) => {
+     if(!req.params.size) req.params.size = 25;
+
+     let response = { response: []};
+     for(let i=0; i<req.params.size; i++){
+       let id = uuid();
+        response.response.push({
+          name: id,
+          src: 'https://robohash.org/{$id}'
+        });
+     }
+    
+    res.send('hola')
+   })
    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
