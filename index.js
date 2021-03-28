95b5210114b58b6dea6dabf791762b4b7dc16b3b
gerardfp <gerardfp@gmail.com>
2021-03-28 22:35:31 +0200

    .

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 const express = require('express');
-var uuid = require("uuid");
+const { v4: uuidv4,} = require('uuid');
 
 const PORT = process.env.PORT || 5000
 
- express()
-   .get('/:size?', (req, res) => {
-     if(!req.params.size) req.params.size = 25;
 
-     let response = { response: []};
-     for(let i=0; i<req.params.size; i++){
-       let id = uuid();
-        response.response.push({
-          name: id,
-          src: 'https://robohash.org/{$id}'
-        });
-     }
-    
-    res.send(response);
-   })
+// https://testimonialapi.toolcarton.com/api
+
+function template(strings, ...keys) {
+  return (function(...values) {
+    let dict = values[values.length - 1] || {};
+    let result = [strings[0]];
+    keys.forEach(function(key, i) {
+      let value = Number.isInteger(key) ? values[key] : dict[key];
+      result.push(value, strings[i + 1]);
+    });
+    return result.join('');
+  });
+}
+
+const genId = (size, src) => {
+  let response = { response: []};
+  size = size ? size : 25;
+  for(let i=0; i<size; i++){
+    let id = uuidv4();
+     response.response.push({
+       id: id,
+       name: id.replace(/-/g, ' '),
+       shortname: id.replace(/-.*/, ''),
+       src: src({id: id}),
+       likes: 0
+     });
+  }
+  return response;
+}
+
+express()
+  .get('/robots/:size?', (req, res) => {
+    res.send(genId(req.params.size, template`https://robohash.org/${'id'}`));
+  })
+  .get('/monsters/:size?', (req, res) => {
+    res.send(genId(req.params.size, template`https://robohash.org/${'id'}?set=set2`));
+  })
+  .get('/kittens/:size?', (req, res) => {
+    res.send(genId(req.params.size, template`https://robohash.org/${'id'}?set=set4`));
+  })
+  .get('/humans/:size?', (req, res) => {
+    res.send(genId(req.params.size, template`https://robohash.org/${'id'}?set=set5`));
+  })
+
+  .get('/likes/:id?', (req, res) => {
+   res.send({ likes: Math.floor(Math.random()*2000 + 1)});
+  })
+
    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
