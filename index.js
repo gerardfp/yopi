06001732d0fdce13e76a8f40526f202d234bf051
gerardfp <gerardfp@gmail.com>
2021-05-18 11:29:01 +0200

    .

--- index.js
+++ index.js
@@ -7,20 +7,6 @@ const PORT = process.env.PORT || 5000
 
 // https://testimonialapi.toolcarton.com/api
 
-storage = multer.diskStorage({
-  destination: './uploads/',
-  filename: function(req, file, cb) {
-    return crypto2.pseudoRandomBytes(16, function(err, raw) {
-      if (err) {
-        return cb(err);
-      }
-      return cb(null, "" + (raw.toString('hex')) + (path.extname(file.originalname)));
-    });
-  }
-});
-
-
-
 
 
 function template(strings, ...keys) {
@@ -69,9 +55,12 @@ express()
    res.send({ likes: Math.floor(Math.random()*2000 + 1)});
   })
 
-  .post("/upload", multer({storage: storage}).single('avatar'), function(req, res) {
+  .post("/upload", multer({storage:  multer.diskStorage({
+    destination: (req, file, cb) => cb(null, './uploads/'),
+    filename: (req, file, cb) => cb(null, uuidv4())
+  })}).single('avatar'), function(req, res) {
       // res.redirect("/uploads/" + req.file.filename);
-      console.log(req.file.filename);
+      console.log(req.files);
       return res.status(200).end();
     })
     
