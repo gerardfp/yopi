9518368dca635b365d4e5bc1f901aa2cb92de436
gerardfp <gerardfp@gmail.com>
2021-05-18 17:40:31 +0200

    .

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ express()
   })
     
   .get('/uploads/:upload', async function (req, res){
-    const data = await db.any("SELECT data FROM files where name = ${name}", {name: req.params.upload});
+    const data = await db.any("SELECT encode(data,'base64') FROM files where name = ${name}", {name: req.params.upload});
     res.writeHead(200, {'Content-Type': 'image/png', 'Content-Length': data.length });
     res.end(data);
   })
