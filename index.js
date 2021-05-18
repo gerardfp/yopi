89e6221fe731eb94d137f2ae4dcced31ebe775a7
gerardfp <gerardfp@gmail.com>
2021-05-18 11:13:31 +0200

    .

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const { v4: uuidv4,} = require('uuid');
+const multer = require('multer')
 
 const PORT = process.env.PORT || 5000
 
 
 // https://testimonialapi.toolcarton.com/api
+
+storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: function(req, file, cb) {
+    return crypto2.pseudoRandomBytes(16, function(err, raw) {
+      if (err) {
+        return cb(err);
+      }
+      return cb(null, "" + (raw.toString('hex')) + (path.extname(file.originalname)));
+    });
+  }
+});
+
+
+
+
 
 function template(strings, ...keys) {
   return (function(...values) {
@@ -51,5 +68,24 @@ express()
   .get('/likes/:id?', (req, res) => {
    res.send({ likes: Math.floor(Math.random()*2000 + 1)});
   })
+
+  .post("/upload", multer({
+    storage: storage
+    }).single('upload'), function(req, res) {
+      console.log(req.file);
+      console.log(req.body);
+      res.redirect("/uploads/" + req.file.filename);
+      console.log(req.file.filename);
+      return res.status(200).end();
+    })
+    
+    .get('/uploads/:upload', function (req, res){
+    file = req.params.upload;
+    console.log(req.params.upload);
+    var img = fs.readFileSync(__dirname + "/uploads/" + file);
+    res.writeHead(200, {'Content-Type': 'image/png' });
+    res.end(img, 'binary');
+    
+    })
 
    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
